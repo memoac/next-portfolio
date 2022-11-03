6dd88b18d2602c7166d56bf73f67ab5d3315670e
memoac <guillermoaguilarc@gmail.com>
2022-11-03 11:27:11 -0500

    Added Navbar wit inks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+
 const Navbar = () => {
     return (
-        <>
-        Navbar
-        </>
+        <div className="fixed w-full h-20 shadow-xl z-[100] p-5">
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+                <Image src="/../public/assets/navLogo.png" alt="" width='125' height='39' />
+                <div>
+                    <ul className="hidden md:flex">
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+                        </Link>
+                    </ul>
+                </div>
+            </div>    
+        </div>
     );
 }
  
