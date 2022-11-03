aa1bfcd46073f24ff46fa2cca3eb779be8e09dcc
memoac <guillermoaguilarc@gmail.com>
2022-11-03 17:45:42 -0500

    Updated menu animation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,113 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import React, {useState} from "react";
 
 const Navbar = () => {
-    return (
-        <div className="fixed w-full h-20 shadow-xl z-[100] p-5">
-            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-                <Image src="/../public/assets/navLogo.png" alt="" width='125' height='39' />
-                <div>
-                    <ul className="hidden md:flex">
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">About</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
-                        </Link>
-                    </ul>
-                </div>
-            </div>    
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+       setNav(!nav); 
+    }
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100] p-5">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/navLogo.png"
+          alt=""
+          width="125"
+          height="39"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
         </div>
-    );
-}
- 
+      </div>
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div className={nav ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-1000': 
+                                'fixed left-[-100%] top-0 ease-in duration-1000'}>
+          <div className="flex w-full items-center justify-between">
+            <Image
+              src="/../public/assets/navLogo.png"
+              alt="/"
+              width="86"
+              height="27"
+            />
+            <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <AiOutlineClose />
+            </div>
+          </div>
+          <div className="border-b border-gray-300 my-4">
+            <p className="w-[85%] md:w-[90%] py-4">
+              Let's build domething legendary together
+            </p>
+          </div>
+
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="">
+                <li className="py-4 tet-sm">Home</li>
+              </Link>
+              <Link href="">
+                <li className="py-4 tet-sm">About</li>
+              </Link>
+              <Link href="">
+                <li className="py-4 tet-sm">Skills</li>
+              </Link>
+              <Link href="">
+                <li className="py-4 tet-sm">Projects</li>
+              </Link>
+              <Link href="">
+                <li className="py-4 tet-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
