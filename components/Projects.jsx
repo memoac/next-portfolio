6a38e2177c144b1ef2b3fcedddfecfc4b152db38
memoac <guillermoaguilarc@gmail.com>
2022-11-09 17:50:14 -0500

    Added Projets and ProjectItem Component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+
 import propertyImg from "../public/assets/projects/property.jpeg"
+import cryptoImg from "../public/assets/projects/crypto.jpeg";
+import netflixImg from "../public/assets/projects/netflix.jpeg";
+import twitchImg from "../public/assets/projects/twitch.jpeg";
+
+import Projectitem from "./Projectitem";
 const Projects = () => {
     return (
         <div className="w-full">
@@ -9,28 +14,10 @@ const Projects = () => {
                 </p>
                 <h2 className='py-4'>What I&apos;ve Built</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    <div
-                        className="relative 
-                        flex 
-                        items-center 
-                        justify-center 
-                        h-auto 
-                        w-full
-                        shadow-xl
-                        shadow-gray-400 
-                        rounded-xl 
-                        p-4 
-                        group 
-                        hover:bg-gradient-to-r from-[#5651e5] to [#709dff]">
-                        <Image 
-                            className="
-                                rounded-xl 
-                                group-hover:opacity-10 
-                                transition-opacity 
-                                ease-in 
-                                duration-100"
-                            src={propertyImg} alt="/"></Image>
-                    </div>
+                    <Projectitem title="Property Finder" backgroundImg={propertyImg} projectUrl="/property" />
+                    <Projectitem title="Crypto App" backgroundImg={cryptoImg} projectUrl="/property" />
+                    <Projectitem title="Netflix" backgroundImg={netflixImg} projectUrl="/property" />
+                    <Projectitem title="Twitch" backgroundImg={twitchImg} projectUrl="/property" />
                 </div>
             </div>
         </div>
